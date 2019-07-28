b25d7fea28322d50a815a0000177dd8d9aea4d80
akatkar <alikatkar@gmail.com>
2019-07-28 16:09:14 +0300

    type problems fixed

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,7 +7,7 @@ import {Post, PostService} from '../services/post.service';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts = [];
+  posts: Post[];
 
   constructor(private service: PostService) {
   }
@@ -16,17 +16,23 @@ export class PostsComponent implements OnInit {
     console.log('here we are');
     this.service.getPosts()
       .subscribe(response => {
-        console.log(response);
+        this.posts = response;
+        console.log(this.posts);
       }, error => {
         alert('An unexpected error occured');
       });
   }
 
-  createPost(title: HTMLInputElement) {
-    let post = {title: title.value};
+  createPost(input: HTMLInputElement) {
+    const post = {title: input.value};
+    input.value = '';
+
     this.service.createPost(post)
       .subscribe(response => {
         console.log(response);
+        const savedPost = {id: response, title: post.title, userId: 1, body: 'new created body' };
+        this.posts.splice(0, 0, savedPost);
+
       }, (error: Response) => {
         if (error.status === 400) {
           console.log('400');
@@ -45,6 +51,22 @@ export class PostsComponent implements OnInit {
 
           console.log('Rrror', error);
 
+        });
+  }
+
+  deletePost(post) {
+    this.service.deletePost(post.id)
+      .subscribe(response => {
+          const index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        },
+        (error: Response) => {
+          if (error.status === 404) {
+            alert('this post has already been deleted');
+          } else {
+            alert('An unexpected error occurred');
+          }
+          console.log('Error', error);
         });
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -18,15 +18,18 @@ export class PostService {
   }
 
   getPosts() {
-    return this.http.get<Post>(this.url);
+    return this.http.get<Post[]>(this.url);
   }
 
   createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post));
+    return this.http.post<number>(this.url, JSON.stringify(post));
   }
 
   updatePost(post) {
-    return this.http.post(this.url + '/' + post.id, JSON.stringify(post));
+    return this.http.post<Post>(this.url + '/' + post.id, JSON.stringify(post));
   }
 
+  deletePost(id) {
+    return this.http.delete(this.url + '/' + id);
+  }
 }
